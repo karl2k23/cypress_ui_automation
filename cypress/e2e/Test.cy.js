5a0c2ceff3960306b136f8ffc33a2e61db3e1cd3
unknown <nasgar@ssct.edu.ph>
2024-05-04 16:24:16 +0800

    test

---
+++ cypress/e2e/Test.cy.js
@@ -0,0 +1,13 @@
+describe('Katalan Application', () =>{
+    it("Home Page", () =>{
+        cy.visit('https://katalon-demo-cura.herokuapp.com/')
+        cy.get('#btn-make-appointment').click();
+        cy.get('#txt-username').type('John Doe');
+        cy.get('#txt-password').type('ThisIsNotAPassword');
+        cy.get('#btn-login').click();
+        cy.get('#chk_hospotal_readmission').click();
+        cy.get('#txt_visit_date').type('04052024');
+        cy.get('#btn-book-appointment').click();
+        
+    })
+})
